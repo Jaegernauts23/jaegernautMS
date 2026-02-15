2bdf9ee15e35dc6978903b756db44d44263e38a9
Bhargav <rambhargav017@gmail.com>
2026-02-16 00:32:03 +0530

    code chagnes

--- src/main/java/com/microservices/services/PasswordServices/PasswordLoginService.java
+++ src/main/java/com/microservices/services/PasswordServices/PasswordLoginService.java
@@ -1,11 +1,21 @@
 package com.microservices.services.PasswordServices;
 
 import com.microservices.models.DAO.PasswordLoginDAO;
+import com.microservices.repositories.PasswordLoginRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PasswordLoginService {
+    
+    @Autowired
+    PasswordLoginRepository passwordLoginRepository;
+    
     public boolean verfiyValidLogin(PasswordLoginDAO passwordLoginDAO){
-        return false;
+        PasswordLoginDAO user = passwordLoginRepository.findByEmailAndPassword(
+            passwordLoginDAO.getEmail(), 
+            passwordLoginDAO.getPassword()
+        );
+        return user != null;
     }
 }
